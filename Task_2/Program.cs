416e31c8032231427bdb85c0e4fd02557122ce7d
sergeylebedev <lebedevs06@gmail.com>
2022-10-15 21:10:23 +0400

    Нахождение большего из трёх

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Введите часло А");
+int namber_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите часло B");
+int namber_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите часло C");
+int namber_3 = Convert.ToInt32(Console.ReadLine());
+int max = namber_1;
+
+if(max < namber_2)
+{
+    max = namber_2;
+}
+if(max < namber_3)
+{
+   max = namber_3;
+}
+Console.WriteLine(max);
